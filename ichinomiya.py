a0feb0065aa8df45134b0737d92bbf2ff3989bf4
Shunyo Ogawa <shunyo44@gmail.com>
2021-05-21 09:11:37 +0900

    modify ichinomiya.py to use utility functions

--- ichinomiya.py
+++ ichinomiya.py
@@ -4,17 +4,11 @@ import urllib.request
 import urllib.parse
 from datetime import datetime, timedelta
 import re
-import time
-import pathlib
 
-import pandas
-from aichi import get_number_by_delta
-from aichi import get_day_of_week_jp
-from utility import generate_post
-from twitter_post import post
+from utility import pre_post, post_city
 
 
-def get_ichinomiya_info() -> dict:
+def get_ichinomiya_info(engine_number=1) -> dict:
 
     # Home page
     url = "https://www.city.ichinomiya.aichi.jp/covid19/1033846/index.html"
@@ -49,54 +43,58 @@ def get_ichinomiya_info() -> dict:
     return {"is_today": is_today, "is_yesterday": is_yesterday, "number": today_number, "url": detailed_url}
 
 
-def get_ichinomiya_last_week():
-    df0 = pandas.read_pickle("database.zip")
-    return get_number_by_delta(df0, -7, region="一宮市")
+# def get_ichinomiya_last_week():
+#     df0 = pandas.read_pickle("database.zip")
+#     return get_number_by_delta(df0, -7, region="一宮市")
 
 
-def post_ichinomiya():
-    today = datetime.today()
-    ichinomiya_info = get_ichinomiya_info()
-    num_last_week = get_ichinomiya_last_week()
-    article_url = ichinomiya_info["url"]
-    num_today = ichinomiya_info["number"]
+# def post_ichinomiya():
+#     today = datetime.today()
+#     ichinomiya_info = get_ichinomiya_info()
+#     num_last_week = get_ichinomiya_last_week()
+#     article_url = ichinomiya_info["url"]
+#     num_today = ichinomiya_info["number"]
 
-    zip_path = pathlib.Path("ichinomiya_lock.zip")
-    if ichinomiya_info["is_today"] and not zip_path.exists() and num_today >= 0:
-        youbi = get_day_of_week_jp(today)
-        header = f'[速報]一宮市の本日の新型コロナウイルスの新規感染者数は{num_today}人(先週の{youbi}に比べて{num_today-num_last_week:+}人)でした。詳細は公式サイトを参照 > {article_url}'
-        # print(header)
-        post(header)
+#     zip_path = pathlib.Path("ichinomiya_lock.zip")
+#     if ichinomiya_info["is_today"] and not zip_path.exists() and num_today >= 0:
+#         youbi = get_day_of_week_jp(today)
+#         header = f'[速報]一宮市の本日の新型コロナウイルスの新規感染者数は{num_today}人(先週の{youbi}に比べて{num_today-num_last_week:+}人)でした。詳細は公式サイトを参照 > {article_url}'
+#         # print(header)
+#         post(header)
 
-        data_for_save = pandas.DataFrame(
-            [{'本日': num_today, '先週': num_last_week}], index=['一宮市'])
-        data_for_save.to_pickle("ichinomiya_lock.zip")
-        time.sleep(5)
-        print("一宮市更新しました", datetime.today())
+#         data_for_save = pandas.DataFrame(
+#             [{'本日': num_today, '先週': num_last_week}], index=['一宮市'])
+#         data_for_save.to_pickle("ichinomiya_lock.zip")
+#         time.sleep(5)
+#         print("一宮市更新しました", datetime.today())
 
 
-def prepost_ichinomiya() -> dict:
-    city = "一宮市"
-    today = datetime.today()
-    ichinomiya_info = get_ichinomiya_info()
-    num_last_week = get_ichinomiya_last_week()
-    num_today = ichinomiya_info["number"]
+# def prepost_ichinomiya() -> dict:
+#     city = "一宮市"
+#     today = datetime.today()
+#     ichinomiya_info = get_ichinomiya_info()
+#     num_last_week = get_ichinomiya_last_week()
+#     num_today = ichinomiya_info["number"]
 
-    zip_path = pathlib.Path("ichinomiya_lock.zip")
-    is_postable = (ichinomiya_info["is_today"]) & (
-        not zip_path.exists()) & (num_today >= 0)
-    info = {"number_last_week": num_last_week,
-            "number_today": num_today,
-            "city": city,
-            "article_url": ichinomiya_info["url"],
-            "weekday": get_day_of_week_jp(today),
-            "zip_path": zip_path,
-            "is_postable": is_postable}
-    info["headline"] = generate_post(info)
-    return info
+#     zip_path = pathlib.Path("ichinomiya_lock.zip")
+#     is_postable = (ichinomiya_info["is_today"]) & (
+#         not zip_path.exists()) & (num_today >= 0)
+#     info = {"number_last_week": num_last_week,
+#             "number_today": num_today,
+#             "city": city,
+#             "article_url": ichinomiya_info["url"],
+#             "weekday": get_day_of_week_jp(today),
+#             "zip_path": zip_path,
+#             "is_postable": is_postable}
+#     info["headline"] = generate_post(info)
+#     return info
+
+
+def post_toyota():
+    post_city(pre_post("一宮市", "ichinomiya_lock.zip", get_ichinomiya_info))
 
 
 if __name__ == "__main__":
     # post_ichinomiya()
     # print(get_ichinomiya_info())
-    print(prepost_ichinomiya())
+    print(pre_post("一宮市", "ichinomiya_lock.zip", get_ichinomiya_info))
